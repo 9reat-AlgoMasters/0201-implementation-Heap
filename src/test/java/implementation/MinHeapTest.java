9eb94a88955829ee0a7dfe80097c54f280efc56d
skydreamer21 <skydreamer210@gmail.com>
2023-03-05 16:50:29 +0900

    test: MinHeap 테스트 생성

--- src/test/java/implementation/MinHeapTest.java
+++ src/test/java/implementation/MinHeapTest.java
@@ -1,7 +1,62 @@
 package implementation;
 
-import static org.junit.jupiter.api.Assertions.*;
+import exceptions.CustomNoSuchElementException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.*;
 
 class MinHeapTest {
-
+    static MinHeap heap;
+    
+    @BeforeEach
+    void beforeEach() {
+        heap = new MinHeap();
+        heap.add(3);
+        heap.add(10);
+        heap.add(2);
+        heap.add(7);
+        heap.add(3);
+    }
+    
+    @DisplayName("isEmpty() 테스트 - True")
+    @Test
+    void isEmptyTest1() {
+        MinHeap emptyHeap = new MinHeap();
+        assertThat(emptyHeap.isEmpty()).isTrue();
+    }
+    
+    @DisplayName("isEmpty() 테스트 - false")
+    @Test
+    void isEmptyTest2() {
+        assertThat(heap.isEmpty()).isFalse();
+    }
+    
+    @DisplayName("remove 테스트")
+    @Test
+    void deleteTest1() {
+        assertThat(heap.remove()).isEqualTo(2);
+        assertThat(heap.size()).isEqualTo(4);
+        
+        assertThat(heap.remove()).isEqualTo(3);
+        assertThat(heap.size()).isEqualTo(3);
+        
+        assertThat(heap.remove()).isEqualTo(3);
+        assertThat(heap.size()).isEqualTo(2);
+        
+        assertThat(heap.remove()).isEqualTo(7);
+        assertThat(heap.size()).isEqualTo(1);
+        
+        assertThat(heap.remove()).isEqualTo(10);
+        assertThat(heap.size()).isEqualTo(0);
+    }
+    
+    @DisplayName("remove 테스트 - 비어있을 때 예외 발생")
+    @Test
+    void deleteTest2() {
+        MinHeap emptyHeap = new MinHeap();
+        assertThrows(CustomNoSuchElementException.class, emptyHeap::remove);
+    }
 }
