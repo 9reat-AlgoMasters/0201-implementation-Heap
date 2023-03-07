b4e2c22c7920990d9291c5bf827ec0975beb5200
itsdangerous <danger121@naver.com>
2023-03-07 11:14:09 +0900

    impl: MaxHeap

--- src/main/java/implementation/MaxHeap.java
+++ src/main/java/implementation/MaxHeap.java
@@ -1,29 +1,87 @@
 package implementation;
 
+import exceptions.CustomNoSuchElementException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MaxHeap implements Heap {
-    
+    int size;
+    List<Integer> element;
+    public MaxHeap() {
+        this.size = 0;
+        this.element = new ArrayList<>();
+    }
+
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
     
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
     
     @Override
     public void add(int num) {
-    
+        element.add(num);
+        size++;
+        makeHeap(size);
     }
     
     @Override
     public int remove() {
-        return 0;
+        if (size == 0) {
+            throw new CustomNoSuchElementException();
+        }
+        else {
+            int k = element.remove(0);
+            size--;
+            makeHeap(size);
+            return k;
+        }
     }
     
     @Override
     public void clear() {
-    
+        this.size = 0;
+        this.element = new ArrayList<>();
+
+    }
+
+    public void heapify(int parent, int size) {
+        int leftChild = parent * 2 + 1;
+        int rightChild = parent * 2 + 2;
+        int maxV = parent;
+        if (leftChild < size && element.get(leftChild) > element.get(maxV)) {
+            maxV = leftChild;
+        }
+        if (rightChild < size && element.get(rightChild) > element.get(maxV)) {
+            maxV = rightChild;
+        }
+        if(maxV != parent) {
+            swap(parent, maxV);
+            heapify(maxV, size);
+        }
+    }
+
+    private void makeHeap(int size) {
+        for (int i = size / 2 - 1; i >= 0; i--) {
+            heapify(i, size);
+        }
+    }
+
+    private void swap(int a, int b) {
+        int tmp = this.element.get(a);
+        element.set(a, element.get(b));
+        element.set(b, tmp);
+    }
+
+    public void printElement() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(element.get(i)+ " ");
+        }
+        System.out.println();
     }
 }
